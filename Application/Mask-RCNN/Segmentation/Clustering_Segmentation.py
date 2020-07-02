a65bc30622116aa3fd98ec5150eaa6875eab468f
Ronit Samaddar <ronitsamaddar97@gmail.com>
2020-07-02 12:34:25 +0530

    Reverting back one commit

--- Application/Mask-RCNN/Segmentation/Clustering_Segmentation.py
+++ Application/Mask-RCNN/Segmentation/Clustering_Segmentation.py
@@ -1,3 +1,41 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3f283311e43724186f4c8b24086c4436c764cb24edc125f81b654230750c040c
-size 1052
+# from skimage.color import rgb2gray
+
+# import matplotlib.pyplot as plt
+# %matplotlib inline
+# from scipy import ndimage
+import cv2
+import argparse
+import numpy as np
+from sklearn.cluster import KMeans
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True,
+                help = 'input image path')
+ap.add_argument('-c', '--num_clusters', required=True,
+                help = 'Number of clusters')
+args = ap.parse_args()
+
+image = cv2.imread(args.image)
+print(image.shape)
+image=image/255
+cv2.imshow("Original Image",image)
+"""
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Grayscale Image",gray)
+print(gray.shape)
+
+"""
+pic_n = image.reshape(image.shape[0]*image.shape[1],image.shape[2])
+print(pic_n.shape)
+
+kmeans = KMeans(n_clusters=int(args.num_clusters), random_state=0).fit(pic_n)
+pic2show = kmeans.cluster_centers_[kmeans.labels_]
+cluster_pic = pic2show.reshape(image.shape[0], image.shape[1], image.shape[2])
+cv2.imshow("Clustereing segmented image",cluster_pic)
+
+
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
